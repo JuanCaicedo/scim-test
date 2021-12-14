e4dfb99f03101abfd93701f0e335411aec995c7b
JuanCaicedo <retiredcanadianpoet@gmail.com>
2021-12-14 14:49:12 -0700

    combine get endpoints

--- app.js
+++ app.js
@@ -16,15 +16,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/Users", (req, res, next) => {
-  const { path, body, query } = req;
-  console.log("IN GET USERS ENDPOINT");
+app.get("/Users/:userId", (req, res, next) => {
+  const { path, body, query, params } = req;
+  console.log("IN GET USERS ID ENDPOINT");
   console.log("path", path);
   console.log("body", body);
   console.log("query", query);
+  console.log("params", params);
+  if (params?.userId) {
+    const json = {
+      schemas: ["urn:ietf:params:scim:schemas:core:2.0:User"],
+      id: "juan+ssotest12.06.21.5@calm.com",
+      userName: "juan+ssotest12.06.21.5@calm.com",
+      name: {
+        givenName: "Another",
+        middleName: "",
+        familyName: "User",
+      },
+      active: true,
+      groups: [],
+      meta: {
+        resourceType: "User",
+      },
+    };
+    return res.status(200).json(json);
+  }
+
   if (query?.filter !== 'userName eq "juan+ssotest12.06.21.5@calm.com"') {
     return res.status(404);
   }
+
   const json = {
     schemas: ["urn:ietf:params:scim:api:messages:2.0:ListResponse"],
     totalResults: 1,
@@ -49,32 +70,7 @@ app.get("/Users", (req, res, next) => {
     ],
   };
 
-  res.status(200).json(json);
-});
-
-app.get("/Users/:userId", (req, res, next) => {
-  const { path, body, query, params } = req;
-  console.log("IN GET USERS ID ENDPOINT");
-  console.log("path", path);
-  console.log("body", body);
-  console.log("query", query);
-  console.log("params", params);
-  const json = {
-    schemas: ["urn:ietf:params:scim:schemas:core:2.0:User"],
-    id: "juan+ssotest12.06.21.5@calm.com",
-    userName: "juan+ssotest12.06.21.5@calm.com",
-    name: {
-      givenName: "Another",
-      middleName: "",
-      familyName: "User",
-    },
-    active: true,
-    groups: [],
-    meta: {
-      resourceType: "User",
-    },
-  };
-  res.status(200).json(json);
+  return res.status(200).json(json);
 });
 
 app.put("/Users/:userId", (req, res, next) => {
